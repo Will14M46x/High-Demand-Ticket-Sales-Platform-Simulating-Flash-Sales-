4c5f1ee33826dd54d2dd0adc62585a8cae93f524
MaxwellClein22364862 <152290030+MaxwellClein22364862@users.noreply.github.com>
2025-11-10 15:00:03 +0100

    Project Fix to separate branch

---
+++ waiting-room-service/src/main/java/waitingroomapplications/service/WaitingRoomServiceApplication.java
@@ -0,0 +1,12 @@
+package waitingroomapplications.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaitingRoomServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WaitingRoomServiceApplication.class, args);
+    }
+}
